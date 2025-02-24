d1fe508016d017ab5ed5e6c0f4af9862ae3ba10a
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-02-24 20:38:25 +0530

    Updated css page

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll(".navbar a");
+  links.forEach(link => {
+      link.addEventListener("click", function() {
+          links.forEach(item => item.classList.remove("active"));
+          this.classList.add("active");
+      });
+  });
+});
